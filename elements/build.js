7beee462fc1c667103516d2b8d6f3bdf4137d33b
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-11-04 07:56:28 -0700

    additions to complete package

--- elements/build.js
+++ elements/build.js
@@ -16,6 +16,15 @@ const concat = require('concat');
 
     await concat(files, `dist/${project}/${project}.js`);
 
+    if (await fs.exists(`./package.json`))
+        await fs.copyFile(`./package.json`, `dist/${project}/package.json`);
+
+    if (await fs.exists(`./LICENSE`))
+        await fs.copyFile(`./LICENSE`, `dist/${project}/LICENSE`);
+
+    if (await fs.exists(`./README.md`))
+        await fs.copyFile(`./README.md`, `dist/${project}/README.md`);
+
     if (await fs.exists(`./dist/${project}-build/styles.css`))
         await fs.copyFile(`./dist/${project}-build/styles.css`, `dist/${project}/${project}.css`);
 
